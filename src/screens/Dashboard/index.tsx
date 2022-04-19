6194774d7326e2a301a033471bb4520d794b0240
JA-Lourenco <ja.lourenco@outlook.com>
2022-04-19 10:21:09 -0300

    Dashboard: Styling the logged user section

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 
-import { Container, Title } from './styles'
+import { 
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper
+} from './styles'
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{uri: 'https://avatars.githubusercontent.com/u/93841387?v=4'}}
+                        />
+                        
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Rodrigo</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
